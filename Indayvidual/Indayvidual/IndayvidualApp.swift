60c48e04c5a44470b50d77227ffcd98564efe808
Juri-Jang <jjr84@dgu.ac.kr>
2025-07-05 22:17:54 +0900

    🎉 [Chore] 프로젝트 초기 세팅

---
+++ Indayvidual/Indayvidual/IndayvidualApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndayvidualApp.swift
+//  Indayvidual
+//
+//  Created by 장주리 on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct IndayvidualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

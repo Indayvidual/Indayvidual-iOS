82ff581f313880d9b6fc525a167927b2056165eb
Jimin <gift040404@naver.com>
2025-07-28 16:06:44 +0900

    ✨ [Feature] category DTO 생성 #50
    - DTO 폴더 생성
    - Request/Response DTO 파일 생성

---
+++ Indayvidual/Indayvidual/Sources/Features/TodoList/Model/DTO/CategoryRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryRequestDTO.swift
+//  Indayvidual
+//
+//  Created by 김지민 on 7/28/25.
+//
+
+import Foundation
+
+struct CategoryRequestDTO: Encodable {
+    let name: String
+    let color: String
+}

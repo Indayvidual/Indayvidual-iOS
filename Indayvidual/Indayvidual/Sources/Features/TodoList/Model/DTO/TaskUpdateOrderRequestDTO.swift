bf2326af3cf9feb01303c93c3f466da283fdb895
Jimin <gift040404@naver.com>
2025-07-29 20:21:42 +0900

    ✨ [Feature] taskDTO 정의 #54

---
+++ Indayvidual/Indayvidual/Sources/Features/TodoList/Model/DTO/TaskUpdateOrderRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  TaskUpdateOrderRequestDTO.swift
+//  Indayvidual
+//
+//  Created by 김지민 on 7/28/25.
+//
+
+import Foundation
+
+struct TaskUpdateOrderRequestDTO : Encodable {
+    let title : [Int]
+}

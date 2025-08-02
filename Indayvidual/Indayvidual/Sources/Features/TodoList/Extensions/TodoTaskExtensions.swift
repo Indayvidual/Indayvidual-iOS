167c085dbe4c1e3f1e01732199a78ed262436885
Jimin <gift040404@naver.com>
2025-08-02 20:33:07 +0900

    ✨ [Feat] TodolistView - 카테고리 삭제 관련 extension #28
    - 카테고리 삭제 관련 extension : TodoViewModel extension 추가

--- Indayvidual/Indayvidual/Sources/Features/TodoList/Extensions/TodoTaskExtensions.swift
+++ Indayvidual/Indayvidual/Sources/Features/TodoList/Extensions/TodoTaskExtensions.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension CheckListItem {
     func toTodoTask(categoryId: Int, date: String, order: Int) -> TodoTask {
@@ -23,5 +24,29 @@ extension CheckListItem {
 extension TodoTask {
     func toCheckListItem() -> CheckListItem {
         return CheckListItem(text: self.title, isChecked: self.isCompleted)
+    }
+}
+
+extension TodoViewModel {
+    // 카테고리 이름, 색상 수정
+    func updateCategory(_ category: Category, newName: String, newColor: Color) {
+        guard let index = categories.firstIndex(where: { $0.categoryId == category.categoryId }) else { return }
+        categories[index] = Category(
+            categoryId: category.categoryId,
+            name: newName,
+            color: newColor
+        )
+        // TODO: API
+    }
+    
+    // 카테고리 삭제 + 삭제 할 카테고리에 있는 task들도 모두 삭제
+    func deleteCategory(_ category: Category) {
+        // 1. 카테고리 제거
+        categories.removeAll { $0.categoryId == category.categoryId }
+        // 2. 모든 날짜에서 해당 카테고리의 task 제거
+        for key in tasks.keys {
+            tasks[key]?.removeAll { $0.categoryId == category.categoryId }
+        }
+        // TODO: API
     }
 }

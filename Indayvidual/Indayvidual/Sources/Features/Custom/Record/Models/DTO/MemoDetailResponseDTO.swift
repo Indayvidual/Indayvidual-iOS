37b9f59e693b1af4d368cde453e252b304863346
Doyeon Kim <ehdus022191@dgu.ac.kr>
2025-08-09 17:06:34 +0900

    [Fix] MemoPreview

--- Indayvidual/Indayvidual/Sources/Features/Custom/Record/Models/DTO/MemoDetailResponseDTO.swift
+++ Indayvidual/Indayvidual/Sources/Features/Custom/Record/Models/DTO/MemoDetailResponseDTO.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct MemoDetailResponseDTO: Codable {
     let memoId: Int
+    let title: String
     let content: String
     let createdDate: String
     let createdTime: String
@@ -16,19 +17,13 @@ struct LocalTime: Codable {
 
 extension MemoDetailResponseDTO {
     func toModel() -> MemoModel {
-        // "09:35:59.34483839" 형태에서 "09:35"만 추출
-        let timeComponents = createdTime.split(separator: ":")
-        let hour = timeComponents.indices.contains(0) ? String(timeComponents[0]) : "00"
-        let minute = timeComponents.indices.contains(1) ? String(timeComponents[1]) : "00"
-        let formattedTime = "\(hour):\(minute)"
-        
         return MemoModel(
             id: UUID(),
             memoId: memoId,
-            title: "",  // DTO에 title이 없다면 빈 문자열로
+            title: title,
             content: content,
-            date: createdDate,
-            time: formattedTime
+            date: createdDate.asYYMMDD(),
+            time: createdDate.asHHmm()
         )
     }
 }

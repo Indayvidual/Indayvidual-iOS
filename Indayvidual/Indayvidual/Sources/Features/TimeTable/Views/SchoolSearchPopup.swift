3ed6a85bbf29e53ad064352259c00e974d4be266
Juri-Jang <jjr84@dgu.ac.kr>
2025-08-20 13:19:27 +0900

    🐛 [Fix] 중복 선언 제거, 에러 메세지 maxwidth 변경, 스크롤시 키보드 사라지는 기능 추가

--- Indayvidual/Indayvidual/Sources/Features/TimeTable/Views/SchoolSearchPopup.swift
+++ Indayvidual/Indayvidual/Sources/Features/TimeTable/Views/SchoolSearchPopup.swift
@@ -60,7 +60,6 @@ struct SchoolSearchPopup: View {
                                 .frame(maxHeight: 210) // 검색창 등 제외한 나머지 높이
                         }
                     }
-                    .background(Color.white)
                 }
                 
                 .background(Color.white)
@@ -100,13 +99,14 @@ struct SchoolSearchPopup: View {
                     .foregroundColor(viewModel.schoolNames.isEmpty ? .gray : .red)
                 Spacer()
             }
-            .frame(maxWidth: 210)
+            .frame(maxWidth: .infinity)
         } else {
             List(viewModel.schoolNames) { school in
                 SchoolRow(school: school, selectedSchool: $selectedSchool)
                     .listRowSeparator(.hidden)
             }
             .listStyle(.plain)
+            .scrollDismissesKeyboard(.interactively)
         }
         
         
